8555e3676614c2680ca9332287cadec386d8187a
Maria Antici <maria.antici99@gmail.com>
2023-03-15 18:27:10 +0200

    Update app with existing components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import Head from 'next/head'
+import DefaultLayout from '@/layouts/DefaultLayout';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Component library</title>
-        <meta name="description" content="Discover my personal component library built with Next.js and Tailwind CSS" />
+        <title>Component gallery</title>
+        <meta name="description" content="Discover my personal component gallery built with Next.js and Tailwind CSS" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="" />
       </Head>
-      <main>
-        <p>Hello!</p>
-      </main>
+      <DefaultLayout>
+        <h1 className='py-80'>This is H1 heading</h1>
+      </DefaultLayout>
     </>
   )
 }
