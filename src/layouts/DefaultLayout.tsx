3cc283030eaa8bf985cd64f42549183f5dd13b7b
Maria Antici <maria.antici99@gmail.com>
2023-03-24 14:12:42 +0200

    Quick fix for Header

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -11,6 +11,7 @@ export default function DefaultLayout({ children }: DefaultLayoutProps) {
     return (
         <div>
             <Header />
+            <div className="h-16 sm:h-24" />
             <div className='max-w-7xl mx-auto flex flex-row px-6 sm:px-8 xl:px-0'>
                 <div className='w-52'>
                     <Sidebar />
